953124cb77525d7ac7a1c7a853edc83b5430300b
Leonardo Noll <leonoll8@hotmail.com>
2025-03-21 11:24:02 -0300

    fix: removed margin from invalidInputMessage

--- src/components/ui/invalidInputMessage.tsx
+++ src/components/ui/invalidInputMessage.tsx
@@ -1,7 +1,7 @@
 const InvalidInputMessage = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div>
-      <p className="text-red-500 text-sm mt-2">{children}</p>
+      <p className="text-red-500 text-sm ">{children}</p>
     </div>
   );
 };
